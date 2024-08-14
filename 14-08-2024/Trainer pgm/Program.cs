62decb012d471bb211336b4714b2c0c385e9e5ac
DIJOL DAVIS <83902331+88-dijol@users.noreply.github.com>
2024-08-14 12:39:25 +0530

    Add files via upload

---
+++ 14-08-2024/Trainer pgm/Program.cs
@@ -0,0 +1,20 @@
+﻿using C2504VbFirstLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingFundamentalsProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrainerMenu.Menu();
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ 14-08-2024/Trainer pgm/Trainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingFundamentalsProject
+{
+    internal class Trainer
+    {
+        public int id;
+        public string name;
+        public string place;
+        public string skill;
+
+        public Trainer(int _id, string _name, string _place, string _skill)
+        {
+            id = _id;
+            name = _name;
+            place = _place;
+            skill = _skill;
+           
+        }
+
+        public override string ToString()
+        {
+            return $"[id={id},name={name},place={place},skill={skill}]";
+        }
+    }
+}
