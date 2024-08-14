d3ebd4a3577ddff4a79649c3caf19177d61acd69
DIJOL DAVIS <83902331+88-dijol@users.noreply.github.com>
2024-08-15 00:31:28 +0530

    Add files via upload

---
+++ 14-08-2024/Assignments/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+//3.BikeRaceTrack
+//        { Distance: int }
+//-bool IsGt(BikeRaceTrack other)
+//    TestBikeRaceTrack
+
+
+
+using BikeRaceTrackProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRaceTrackProject
+{
+
+    class BikeRaceTrack
+    {
+        public int Distance { get; set; }
+
+        public BikeRaceTrack(int _distance)
+        {
+            this.Distance = _distance;
+        }
+
+        public bool IsTrackGt(BikeRaceTrack other)
+        {
+            return this.Distance > other.Distance;
+        }
+
+        public bool IsTrackEqual(BikeRaceTrack other)
+        {
+            return this.Distance == other.Distance;
+        }
+
+        public override string ToString()
+        {
+            return $"[Distance={this.Distance}]";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BikeRaceTrack firstTrack = new BikeRaceTrack(20);
+            BikeRaceTrack secondTrack = new BikeRaceTrack(500);
+            if (firstTrack.IsTrackGt(secondTrack))
+            {
+                Console.WriteLine($"First track {firstTrack} is greater than Second  track {secondTrack}");
+            }
+            else if (firstTrack.IsTrackEqual(secondTrack))
+            {
+                Console.WriteLine($"First  track {firstTrack} equals Second track {secondTrack}");
+            }
+            else
+            {
+                Console.WriteLine($"First track {firstTrack} is less than Second track {secondTrack}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+
+//op:First track [Distance=20] is less than Second track [Distance=500]
