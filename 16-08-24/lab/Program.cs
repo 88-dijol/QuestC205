66035d00c9e15ca10c8ef3e5a6360033d257b4c7
DIJOL DAVIS <83902331+88-dijol@users.noreply.github.com>
2024-08-16 23:17:35 +0530

    Add files via upload

--- 16-08-24/lab/Program.cs
+++ 16-08-24/lab/Program.cs
@@ -1,277 +1,79 @@
-
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeePro
+namespace MyProject
 {
-  
-    class Employee
-    {
-            public string Name { get; set; }
-            public string Email { get; set; }
-            public int Age { get; set; }
 
-            public override string ToString()
-            {
-                return $"Name: {Name} {Environment.NewLine}" +
-                    $"Email: {Email} {Environment.NewLine}" +
-                    $"Age: {Age}";
-            }
+    enum Convertion
+    {
+        Upper,
+        Lower,
+        Trim
     }
 
     internal class Program
-
     {
-
-        static void Main()
-
+        static void Convert(string text, Convertion action)
         {
+            // Using if-else to perform the conversion
+            if (action == Convertion.Upper)
+                Console.WriteLine(text.ToUpper());
+            else if (action == Convertion.Lower)
+                Console.WriteLine(text.ToLower());
+            else if (action == Convertion.Trim)
+                Console.WriteLine(text.Trim());
 
-            var employees = new Employee[10];
-
-            int count = 0;
-
-            while (true)
-
+            // Using switch to perform the conversion
+            switch (action)
             {
-
-                Console.WriteLine("1. Add new employee");
-
-                Console.WriteLine("2. Search employee");
-
-                Console.Write("Please enter your option: ");
-
-                var option = Console.ReadLine();
-
-                switch (option)
-
-                {
-
-                    case "1":
-
-                        var emp = new Employee();
-
-                        Console.Write("Name: ");
-
-                        emp.Name = Console.ReadLine();
-
-                        Console.Write("Email: ");
-
-                        emp.Email = Console.ReadLine();
-
-                        Console.Write("Age: ");
-
-                        emp.Age = int.Parse(Console.ReadLine());
-
-                        employees[count] = emp;
-
-                        count++;
-
-                        break;
-
-                    case "2":
-
-                        Console.WriteLine("Email: ");
-
-                        var email = Console.ReadLine();
-
-                        for (int i = 0; i < count; i++)
-
-                        {
-
-                            var e = employees[i];
-
-                            if (e.Email == email)
-
-                            {
-
-                                Console.WriteLine(e);
-
-                                break;
-
-                            }
-
-                        }
-
-                        break;
-
-                    default:
-
-                        Console.WriteLine("Invalid option");
-
-                        break;
-
-                }
-
-            }
-
-        }
-
-    }
-
-}
-
-//op:
-//1. Add new employee
-//2.Search employee
-//Please enter your option: 1
-//Name: Dijol
-//Email: dijoldavis00 @gmail.com
-//Age: 23
-//1.Add new employee
-//2.Search employee
-//Please enter your option: 2
-//Email:
-//dijoldavis00 @gmail.com
-//Name: Dijol
-//Email: dijoldavis00 @gmail.com
-//Age: 23
-//1.Add new employee
-//2.Search employee
-//Please enter your option: 3
-//Invalid option
-//1. Add new employee
-//2.Search employee
-//Please enter your option:
-
-===========================================================
-  other
-
-   class Employee
-
-    {
-
-        public string Name { get; set; }
-
-        public string Email { get; set; }
-
-        public int Age { get; set; }
-
-        public override string ToString()
-
-        {
-
-            return $"Name: {Name} {Environment.NewLine}" +
-
-                $"Email: {Email} {Environment.NewLine}" +
-
-                $"Age: {Age}";
-
-        }
-
-    }
-
-    class EmployeeAdd
-    {
-
-       private  Employee[] _employees = new Employee[10];
-
-        private int _count = 0;
-
-
-        public void Add()
-        {
-            var emp = new Employee();
-
-            Console.Write("Name: ");
-
-            emp.Name = Console.ReadLine();
-
-            Console.Write("Email: ");
-
-            emp.Email = Console.ReadLine();
-
-            Console.Write("Age: ");
-
-            emp.Age = int.Parse(Console.ReadLine());
-
-            _employees[_count] = emp;
-
-            _count++;
-        }
-
-        public void Search()
-        {
-            Console.WriteLine("Email: ");
-
-            var email = Console.ReadLine();
-
-            for (int i = 0; i < _count; i++)
-
-            {
-
-                var e = _employees[i];
-
-                if (e.Email == email)
-
-                {
-
-                    Console.WriteLine(e);
-
+                case Convertion.Upper:
+                    Console.WriteLine(text.ToUpper());
                     break;
-
-                }
-
+                case Convertion.Lower:
+                    Console.WriteLine(text.ToLower());
+                    break;
+                case Convertion.Trim:
+                    Console.WriteLine(text.Trim());
+                    break;
+                default:
+                    break;
             }
         }
-
-    }
-
-    internal class Program
-
-    {
 
         static void Main()
-
         {
-            var manager = new EmployeeAdd();
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
 
-            while (true)
+            Console.WriteLine("1. To Upper");
+            Console.WriteLine("2. To Lower");
+            Console.WriteLine("3. To Trim");
 
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            Convertion action;
+
+            switch (choice)
             {
-
-                Console.WriteLine("1. Add new employee");
-
-                Console.WriteLine("2. Search employee");
-
-                Console.Write("Please enter your option: ");
-
-                var option = Console.ReadLine();
-
-                switch (option)
-
-                {
-
-                    case "1":
-
-                        manager.Add();
-
-                        break;
-
-                    case "2":
-
-                       manager.Search();
-
-                        break;
-
-                    default:
-
-                        Console.WriteLine("Invalid option");
-
-                        break;
-
-                }
-
+                case 1:
+                    action = Convertion.Upper;
+                    break;
+                case 2:
+                    action = Convertion.Lower;
+                    break;
+                case 3:
+                    action = Convertion.Trim;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    return;
             }
 
+            Convert(input, action);
         }
-
     }
-
-}
-
